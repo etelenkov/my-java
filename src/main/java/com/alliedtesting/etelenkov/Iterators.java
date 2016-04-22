93c79471126a035b62f1ea77a38318d27d1b11db
etelenkov <etelenkov@alliedtesting.com>
2016-04-22 11:50:30 +0300

    Added Iterators and other little samples

--- src/main/java/com/alliedtesting/etelenkov/Iterators.java
+++ src/main/java/com/alliedtesting/etelenkov/Iterators.java
@@ -1,22 +1,41 @@
 package com.alliedtesting.etelenkov;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.*;
 
 public class Iterators {
     public static void main(String[] args) {
 
-        System.out.println(sum(take(getNonNegativeIntegersIterator(), 11)));
+        //System.out.println(sum(take(getNonNegativeIntegersIterator(), 11)));
 
-//        System.out.println(reduce2(take(getNonNegativeIntegersIterator(), 20),
+//        System.out.println(toList(take(getNonNegativeIntegersIterator(), 11)));
+//
+//        LookAheadIterator it = new LookAheadIterator(take(getNonNegativeIntegersIterator(), 11));
+//        while (it.hasNext()) {
+//            if (it.hasNext()) System.out.print(" peek: " + it.peek());
+//            System.out.print(" next: " + it.next());
+//            if (it.hasNext()) System.out.print(" peek: " + it.peek());
+//            System.out.println();
+//        }
+
+        //        System.out.println(reduce2(take(getNonNegativeIntegersIterator(), 20),
 //                (a, b) -> {
 //                    a.add(b);
 //                    return a;
 //                }, new ArrayList<Integer>()));
+
+        ArrayList<Iterator<Integer>> arrOfIt = new ArrayList<>();
+        arrOfIt.add(take(getNonNegativeIntegersIterator(), 5));
+        arrOfIt.add(primeIntegers(take(getNonNegativeIntegersIterator(), 16)));
+        arrOfIt.add(magicIntegers(take(getNonNegativeIntegersIterator(), 15)));
+        // print initial iterators
+        print(arrOfIt.get(0));
+        print(arrOfIt.get(1));
+        print(arrOfIt.get(2));
+        // print created iterator
+        Iterator<Integer> it = filter2(arrOfIt, (a, b) -> a <= b);
+        while (it.hasNext()) System.out.print(it.next());
+
     }
 
     public static Iterator<Integer> getNonNegativeIntegersIterator() {
@@ -182,22 +201,111 @@ public class Iterators {
     }
 
     public static <T> T reduce3(Iterator<T> it, BiFunction<T, T, T> bf) {
-        // todo: ...
         return reduce2(it, (a, b) -> (a == null) ? b : bf.apply(a, b), null);
     }
 
+    public static <T> List<T> toList(Iterator<T> it) {
+        return reduce2(it, (a, b) -> {
+            a.add(b);
+            return a;
+        }, new ArrayList<T>());
+    }
 
-//    public static String join(Iterator<?> it) {
-//        String res = null;
-//        while (it.hasNext()) {
-//            String curr = it.next().toString();
-//            if (res != null) res = res + curr;
-//            else res = curr;
+    /**
+     * @param collOfIt
+     * @param p        should return true if 1 and 2nd parameters in the right order, otherwise - false
+     * @param <T>
+     * @return
+     */
+    public static <T> Iterator<T> filter2(Collection<Iterator<T>> collOfIt, BiPredicate<? super T, ? super T> p) {
+        return new Iterator<T>() {
+            // array of LookAheadIterator's of initial iterators
+            private ArrayList<LookAheadIterator<T>> iterators = new ArrayList<>();
+
+            // initialization block
+            {
+                for (Iterator<T> it : collOfIt) iterators.add(new LookAheadIterator<T>(it));
+            }
+
+            @Override
+            public boolean hasNext() {
+                boolean hasNextFlag = false;
+
+                for (int i = 0; i < iterators.size(); i++) {
+                    if (iterators.get(i).hasNext()) hasNextFlag = true;
+                    else {
+                        iterators.remove(i); // removing empty iterator from the list
+                        // decrease counter i because of removal current element
+                        // (we need to check this index again, because now here the next element or nothing)
+                        i--; // todo: may be added to previous command as iterators.remove(i--); ???
+                    }
+                }
+
+                return hasNextFlag;
+            }
+
+            @Override
+            public T next() {
+                if (hasNext()) {
+                    // Find index of needed iterator with the next element
+                    // with help of given BiPredicate testing (if returns false - then
+                    int neededInd = 0; // element with index 0 is always here because hasNext() is true
+                    for (int i = 1; i < iterators.size(); i++)
+                        if (!p.test(iterators.get(neededInd).peek(), iterators.get(i).peek()))
+                            neededInd = i;
+                    // Return needed element of needed iterator
+                    return iterators.get(neededInd).next();
+                }
+
+                // Throw NoSuchElementException if there is no more elements in the iterators
+                throw new NoSuchElementException();
+            }
+        };
+    }
+
+}
+
+class LookAheadIterator<T> implements Iterator<T> {
+    private final Iterator<T> initial;
+    private T next = null; // peeked element
+    private boolean peeked = false; // peeked flag
+
+    public LookAheadIterator(Iterator<T> initial) {
+        this.initial = initial;
+    }
+
+    /**
+     * Before every call of peek() method you should check for hasNext() true value
+     * to avoid NoSuchElementException to be thrown if there is no more next
+     * elements available (the same as with a call of next() method of
+     * Iterator<T> implementation)
+     */
+    public T peek() {
+        if (!peeked) {
+            next = initial.next(); // throws NoSuchElementException if the iteration has no more elements
+            peeked = true;
+        }
+        return next;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return peeked || initial.hasNext();
+    }
+
+//    @Override
+//    public T next() {
+//        if (peeked) {
+//            peeked = false; // after return of next, a peeked element will be the other one
+//            return next;
 //        }
-//        return res;
+//        return initial.next();
 //    }
 
-    /*
-    join(Arrays.asList(4, 5, 6).iterator()) -> "456"
-     */
+    @Override
+    public T next() {
+        T result = peek();
+        peeked = false; // after return of the next, a peeked element will be the other one
+        return result;
+    }
 }
